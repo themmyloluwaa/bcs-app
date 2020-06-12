e4976b7af5e7cd6dbbceaf52bb3d0dbeb1a72409
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-06-12 19:38:13 +0100

    added message screens

---
+++ src/screens/messagescreen/RequestScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RequestScreen = props => {
+  console.log(props);
+
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default RequestScreen;
