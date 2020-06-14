05d44cb5e8680e206e6473f85770048ae041f4ad
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-06-14 09:50:43 +0100

    added box shadow to signin screen

--- src/screens/authscreen/SignInScreen.js
+++ src/screens/authscreen/SignInScreen.js
@@ -17,6 +17,18 @@ const SignInScreen = props => {
         style={{ width: 300, height: 200 }}
       />
       {/* </View> */}
+      <View
+        style={[
+          {
+            backgroundColor: appStyles.white,
+            width: "100%",
+            height: "100%",
+            borderTopRightRadius: 24,
+            borderTopLeftRadius: 24
+          },
+          appStyles.boxShadow
+        ]}
+      />
     </View>
   );
 };
@@ -24,15 +36,22 @@ const SignInScreen = props => {
 const styles = StyleSheet.create({
   containerStyle: {
     display: "flex",
-    justifyContent: "center",
+    // justifyContent: "center",
+    height: "100%",
+    width: "100%",
     alignItems: "center",
     flex: 1,
     backgroundColor: appStyles.primary
+  },
+  imageContainer: {
+    // display: "flex",
+    // height: 40,
+    // maxHeight: 80,
+    // flex: 1,
+    backgroundColor: "green",
+    // justifyContent: "flex-start",
+    marginVertical: 64.03
   }
-  // imageContainer: {
-  //   justifyContent: "flex-start",
-  //   marginVertical: 32.03
-  // }
 });
 
 export default SignInScreen;
--- utils/appStyles.js
+++ utils/appStyles.js
@@ -1,6 +1,14 @@
 const appStyles = {
   primary: "#490222",
-  white: "#fff"
+  white: "#fff",
+  boxShadow: {
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    borderWidth: 1
+  }
 };
 
 export { appStyles };
