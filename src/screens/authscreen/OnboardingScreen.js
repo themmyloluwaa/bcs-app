15729994270907fbfa9586449e4860b9340591b0
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-06-14 08:54:02 +0100

    onboarding completed

--- src/screens/authscreen/OnboardingScreen.js
+++ src/screens/authscreen/OnboardingScreen.js
@@ -37,7 +37,7 @@ const illustrations = [
 
 const scrollX = new Animated.Value(0);
 const stepPosition = new Animated.divide(scrollX, width);
-console.log(stepPosition);
+console.log(width, height);
 
 const renderSteps = () => (
   <View style={styles.stepContainer}>
@@ -63,41 +63,52 @@ const OnboardingScreen = () => {
   return (
     <>
       <View style={styles.onboardingContainer}>
-        <FlatList
-          horizontal
-          pagingEnabled
-          scrollEnabled
-          showsHorizontalScrollIndicator={false}
-          snapToAlignment="center"
-          data={illustrations}
-          extraData={data}
-          scrollEventThrottle={16}
-          keyExtractor={(item, i) => `${item.id}`}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                position: "relative",
-                width: width,
-                justifyContent: "center"
-              }}
-            >
-              <Image
-                source={item.source}
-                resizeMode="contain"
-                style={styles.imageStyle}
-              />
-            </View>
-          )}
-          onScroll={Animated.event(
-            [
-              {
-                nativeEvent: { contentOffset: { x: scrollX } }
-              }
-            ],
-            { useNativeEvent: true }
-          )}
-        />
-        <View>{renderSteps()}</View>
+        <View
+          style={{
+            position: "relative",
+            width: width,
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <FlatList
+            horizontal
+            pagingEnabled
+            scrollEnabled
+            showsHorizontalScrollIndicator={false}
+            snapToAlignment="center"
+            data={illustrations}
+            extraData={data}
+            scrollEventThrottle={16}
+            keyExtractor={(item, i) => `${item.id}`}
+            renderItem={({ item }) => (
+              <View
+                style={{
+                  width: width,
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+              >
+                <Image
+                  source={item.source}
+                  resizeMode="contain"
+                  style={styles.imageStyle}
+                />
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.body}>{item.body}</Text>
+              </View>
+            )}
+            onScroll={Animated.event(
+              [
+                {
+                  nativeEvent: { contentOffset: { x: scrollX } }
+                }
+              ],
+              { useNativeEvent: true }
+            )}
+          />
+          <View>{renderSteps()}</View>
+        </View>
       </View>
     </>
   );
@@ -128,10 +139,24 @@ const styles = StyleSheet.create({
     marginHorizontal: 10
   },
   imageStyle: {
-    overflow: "visible",
-    position: "absolute",
-    width: "100%"
-    // height: height / 2
+    width: "80%",
+    height: height / 4,
+    overflow: "visible"
+  },
+  title: {
+    textAlign: "center",
+    fontStyle: "normal",
+    fontWeight: "300",
+    fontSize: 24,
+    lineHeight: 36,
+    color: "#FFFFFF",
+    marginVertical: 20
+  },
+  body: {
+    textAlign: "center",
+    width: "70%",
+    fontSize: 20,
+    color: "#FFFFFF"
   }
 });
 
