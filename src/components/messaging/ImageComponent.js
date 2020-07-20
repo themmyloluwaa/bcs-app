bae94e226732a1b4cc486cc34e6f47301e279b4b
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-07-20 05:10:04 +0100

    onlong press triggering tooltip

--- src/components/messaging/ImageComponent.js
+++ src/components/messaging/ImageComponent.js
@@ -1,47 +1,65 @@
-import React from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableWithoutFeedback
+} from "react-native";
 import { Image, Tooltip } from "react-native-elements";
 import ReadComponent from "./ReadComponent";
 
 const ImageComponent = ({ userId, ...props }) => {
+  const tooltipRef = useRef(null);
+
   return (
-    <View
-      style={{
-        padding: 10
+    <TouchableWithoutFeedback
+      onLongPress={() => {
+        console.log("working");
+        tooltipRef.current.toggleTooltip();
       }}
     >
-      <Tooltip popover={<Text>Download</Text>}>
-        <Image
-          source={{
-            uri:
-              "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg"
-          }}
-          style={{ width: 200, height: 200 }}
-          containerStyle={{
-            marginBottom: 5,
-            padding: 0
-          }}
-          PlaceholderContent={<ActivityIndicator />}
-        />
-      </Tooltip>
       <View
         style={{
-          flexDirection: "row",
-          alignSelf: "flex-end"
+          padding: 10
         }}
       >
-        <Text
+        <Tooltip
+          ref={tooltipRef}
+          toggleOnPress={false}
+          popover={<Text>Download</Text>}
+        >
+          <Image
+            source={{
+              uri:
+                "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg"
+            }}
+            style={{ width: 200, height: 200 }}
+            containerStyle={{
+              marginBottom: 5,
+              padding: 0
+            }}
+            PlaceholderContent={<ActivityIndicator />}
+          />
+        </Tooltip>
+        <View
           style={{
-            color: userId !== 1 ? "#fafefe" : "#000",
-            paddingRight: 5,
-            fontSize: 10
+            flexDirection: "row",
+            alignSelf: "flex-end"
           }}
         >
-          {props.b}
-        </Text>
-        {userId !== 1 && <ReadComponent read={true} />}
+          <Text
+            style={{
+              color: userId !== 1 ? "#fafefe" : "#000",
+              paddingRight: 5,
+              fontSize: 10
+            }}
+          >
+            {props.b}
+          </Text>
+          {userId !== 1 && <ReadComponent read={true} />}
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
