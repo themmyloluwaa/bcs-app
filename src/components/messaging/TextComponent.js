b7a6afdfc4af90121c4fbb6ff6b658bd513bb500
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-07-21 21:55:20 +0100

    modified text tooltip to trigger on long press

--- src/components/messaging/TextComponent.js
+++ src/components/messaging/TextComponent.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { Text, View, Alert } from "react-native";
+import { Text, View, Alert, TouchableWithoutFeedback } from "react-native";
 import { Tooltip } from "react-native-elements";
 import { copyText } from "../../../utils/clipboardUtil";
 import ReadComponent from "./ReadComponent";
@@ -8,70 +8,77 @@ const TextComponent = ({ userId, ...props }) => {
   const tooltipRef = useRef(null);
 
   return (
-    <View
-      style={{
-        padding: 20
+    <TouchableWithoutFeedback
+      onLongPress={() => {
+        tooltipRef.current.toggleTooltip();
       }}
     >
-      <Tooltip
-        ref={tooltipRef}
-        popover={
-          <Text
-            onPress={() => {
-              copyText("temi");
-              Alert.alert("Feedback", "Copied to clipboard", [
-                {
-                  text: "Ok",
-                  onPress: () => tooltipRef.current.toggleTooltip()
-                }
-              ]);
-            }}
-          >
-            Copy
-          </Text>
-        }
-      >
-        <Text
-          style={{
-            color: userId !== 1 ? "#fff" : "#000",
-            marginBottom: 10
-          }}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
-        </Text>
-      </Tooltip>
-
       <View
         style={{
-          flexDirection: "row",
-          alignSelf: "flex-end"
+          padding: 20
         }}
       >
-        <Text
+        <Tooltip
+          ref={tooltipRef}
+          toggleOnPress={false}
+          popover={
+            <Text
+              onPress={() => {
+                copyText("temi");
+                Alert.alert("Feedback", "Copied to clipboard", [
+                  {
+                    text: "Ok",
+                    onPress: () => tooltipRef.current.toggleTooltip()
+                  }
+                ]);
+              }}
+            >
+              Copy
+            </Text>
+          }
+        >
+          <Text
+            style={{
+              color: userId !== 1 ? "#fff" : "#000",
+              marginBottom: 10
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </Text>
+        </Tooltip>
+
+        <View
           style={{
-            color: userId !== 1 ? "#fafefe" : "#000",
-            paddingRight: 5,
-            fontSize: 10
+            flexDirection: "row",
+            alignSelf: "flex-end"
           }}
         >
-          {props.b}
-        </Text>
-        {userId !== 1 && <ReadComponent read={true} />}
+          <Text
+            style={{
+              color: userId !== 1 ? "#fafefe" : "#000",
+              paddingRight: 5,
+              fontSize: 10
+            }}
+          >
+            {props.b}
+          </Text>
+          {userId !== 1 && <ReadComponent read={true} />}
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
